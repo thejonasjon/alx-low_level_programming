4e2dfbf3b3db2d9cc35ad20fb200828f92a5ce56
Jonazy <jonas.humenu@gmail.com>
2023-02-16 19:34:55 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Description: A C program that prints with put function
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
