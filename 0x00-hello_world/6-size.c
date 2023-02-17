3f6475862f5b7be5153875d9dc29c1c6b57f9d8b
Jonazy <jonas.humenu@gmail.com>
2023-02-16 20:47:17 -0800

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,7 @@ int main(void)
 	printf("Size of a long int: %s byte(s)\n" sizeof(long int));
 	printf("Size of a long long int: %s byte(s)\n" sizeof(long long int);
 	printf("Size of a float: %s byte(s)\n" sizeof(float);
-	return (0);
+
+	return 0;
 }
 
