dc98f00d1d5da67b44330e21951748d6a3b36016
Jonazy <jonas.humenu@gmail.com>
2023-02-16 21:05:15 -0800

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
 /**
  * Description: A C program that prints with put function
 */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");	
+	return (0);
 }
