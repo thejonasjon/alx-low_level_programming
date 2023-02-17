b516169bb69a4c1114589acb4fc2992a06db6240
Jonazy <jonas.humenu@gmail.com>
2023-02-16 19:40:38 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include<stdin.h>
+/* C program that prints exactly with proper grammar, but the outcome is a piece of art */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
