dee7ed35a445c4ef0e1d301dc5df5c0a96ddc277
jonas <jonas.humenu@gmail.com>
2023-02-21 04:03:57 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,13 @@ int main(void)
 
 	for (first_digit = 0; first_digit < 9; first_digit++)
 	{
-		for (second_digit = second_digit + 1; second_digit < 10; second_digit++)
+		for (second_digit = first_digit + 1; second_digit < 10; second_digit++)
 		{
 			putchar((first_digit % 10) + '0');
 			putchar((second_digit % 10) + '0');
 
 			if (first_digit == 8 && second_digit == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
