319686faf504c417e64275efdfca382376723ff2
Jonazy <jonas.humenu@gmail.com>
2023-02-20 18:55:51 -0800

    error fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 {
 	int first_digit = 0, second_digit = 1;
 
-	while (first_digit <= 8)
+	while (first_digit < 9)
 	{
-		while (second_digit <= 9)
+		while (second_digit < 10)
 		{
-			if (first_digit != second_digit && first_digit < second_digit)
+			if (first_digit != second_digit)
 			{
-				putchar(first_digit + 48);
-				putchar(second_digit + 48);
+				putchar(first_digit);
+				putchar(second_digit);
 
 				if (first_digit + second_digit != 17)
 				{
