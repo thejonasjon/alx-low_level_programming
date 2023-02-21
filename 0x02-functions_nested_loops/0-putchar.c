7e220fef00666640845ddc7827c71c5764f9d8cb
Jonazy <jonas.humenu@gmail.com>
2023-02-21 15:13:35 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Desciption: program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	printf("_putchar\n");
+
+	return (0);
+}
