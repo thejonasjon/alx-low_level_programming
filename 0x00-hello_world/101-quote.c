c9e59e80a6ef2ba29a32215aa61f0ab18d6f7f1b
jonas <jonas.humenu@gmail.com>
2023-02-21 05:16:28 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,19 @@
 #include<unistd.h>
+#include<stdio.h>
 
-/* C program that print a quote */
+/**
+ * main - Entry point
+ *
+ * Description: program that print a quot
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-
+	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(1, quote, strlen(quote));
 
-	return 1;
+	return (1);
 }
