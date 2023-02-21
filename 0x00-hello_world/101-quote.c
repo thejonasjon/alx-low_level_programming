c9c10fd14dc9779e954778fc90293e4b7e0ce204
jonas <jonas.humenu@gmail.com>
2023-02-21 05:23:10 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,8 @@
 
 int main(void)
 {	
-	write(1, 2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+// 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
