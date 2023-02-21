75c80269a26131bdbb7182ba63ea51f992aa25e9
Jonazy <jonas.humenu@gmail.com>
2023-02-20 19:14:05 -0800

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program to print all possible different
+ *              combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int first_digit, second_digit, third_digit;
+
+	for (first_digit = 0; first_digit < 9; first_digit++)
+	{
+		for (second_digit = first_digit + 1; second_digit < 9; second_digit++)
+		{
+			for (third_digit = second_digit + 1; third_digit < 10; third_digit++)
+			putchar((first_digit % 10) + '0');
+			putchar((second_digit % 10) + '0');
+
+			if (first_digit + second_digit == 17)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
