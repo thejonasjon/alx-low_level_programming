99634fa63109203f310cb31833b95753883f0ca6
jonas <jonas.humenu@gmail.com>
2023-02-21 04:16:54 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,18 +13,20 @@ int main(void)
 {
 	int first_digit, second_digit, third_digit;
 
-	for (first_digit = 0; first_digit < 9; first_digit++)
+	for (first_digit = 0; first_digit < 8; first_digit++)
 	{
 		for (second_digit = first_digit + 1; second_digit < 9; second_digit++)
 		{
 			for (third_digit = second_digit + 1; third_digit < 10; third_digit++)
-			putchar((first_digit % 10) + '0');
-			putchar((second_digit % 10) + '0');
+				putchar((first_digit % 10) + '0');
+				putchar((second_digit % 10) + '0');
+				putchar((second_digit % 10) + '0');
 
-			if (first_digit + second_digit == 17)
-				continue;
-			putchar(',');
-			putchar(' ');
+				if (first_digit + second_digit + third == 24)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
