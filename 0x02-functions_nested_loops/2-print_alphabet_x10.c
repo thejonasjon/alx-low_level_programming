289438600945f9fb83e2de54c31e625006f42310
Jonazy <jonas.humenu@gmail.com>
2023-02-21 16:28:34 -0800

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ *print_alphabet_x10 - Entry point
+ *
+ * Description: function that prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int _times, ch;
+
+	for (_times = 0; _times < 10; ++_times)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
