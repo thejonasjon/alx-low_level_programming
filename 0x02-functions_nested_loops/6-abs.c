3455707444690a9c40df22d9976fd49b2633465a
Jonazy <jonas.humenu@gmail.com>
2023-02-22 08:31:08 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include <stdlib.h>
+
+/**
+ *_abs: fuction to return absolute value
+ *
+ *@n: argument to return convert value 
+ *	into absolute
+ *
+ *Return: Always n absolute
+ */
+
+int _abs(int n)
+{
+	/*abs function to compute absolute value*/
+	return (abs(n));
+}
