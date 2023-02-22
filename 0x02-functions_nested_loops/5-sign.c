21f90b6b434ce8fd9d0082da5bd70f446350d31a
Jonazy <jonas.humenu@gmail.com>
2023-02-22 08:12:43 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include"mian.h"
+
+/**
+ * print_sign: check if n is a sign
+ *
+ * @n: argument to check for sign
+ *
+ * Return: 1 if n > 0
+ * 	0 if n is 0
+ * 	-1 if n < 0
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
