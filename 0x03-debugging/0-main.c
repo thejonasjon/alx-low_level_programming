bb5bb213c7c3ba43098f1335f145e914b70bb149
Jonazy <jonas.humenu@gmail.com>
2023-02-23 09:03:56 -0800

    0-main, main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+* main - main file is a test for a postitive_or_negative()
+* function
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
