a12b8f4c2acf2e5df9f9d6487791b176bb7126e0
Jonazy <jonas.humenu@gmail.com>
2023-02-22 16:28:40 -0800

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: program that finds and prints the sum of the even-valued terms
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long int j, k, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = a + b;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		a = b;
+		b = sum;
+	}
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
