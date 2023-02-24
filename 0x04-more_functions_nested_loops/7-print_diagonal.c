dbe51e3cf77f223f640807f897a16c76df9dae2c
Jonazy <jonas.humenu@gmail.com>
2023-02-23 21:08:20 -0800

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,20 @@ void print_diagonal(int n)
 	int p, s;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (p = 1; p <= n; ++p)
 		{
 			for (s = 1; s <= p; ++s)
+			{
 				_putchar(' ');
+			}
 			_putchar(92);
 			_putchar('\n');
 		}
 	}
+
 }
