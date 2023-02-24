0288085d52d270e19f2a6c2bf9d2175a9b04ee2b
Jonazy <jonas.humenu@gmail.com>
2023-02-23 20:53:01 -0800

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,8 @@
 #include"main.h"
 
 /**
- * more_numbers - function that prints 10 times the numbers,
- *		from 0 to 14
- *
- * Return: Always 0 (success)
+ * more_numbers - function that prints 10
+ * times the numbers, from 0 to 14
  */
 
 void more_numbers(void)
