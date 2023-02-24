4b7ff5192a1744ec595611e618c6a32b9c178a2b
Jonazy <jonas.humenu@gmail.com>
2023-02-23 20:44:37 -0800

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include"main.h"
 
 /**
- * print_most_numbers - function that prints the numbers, from 0 to 9
+ * print_most_numbers - function that prints the numbers
+ *		from 0 to 9
  *
  * Return: Always 0 (success)
  */
