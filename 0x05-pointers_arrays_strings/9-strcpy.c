13ba31c00848d90ef9fd011a9fadc1211cced945
Jonazy <jonas.humenu@gmail.com>
2023-02-28 20:16:37 -0800

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,7 +14,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
-	printf("%s\n", dest);
-	return (0);
+	int a, b = 0;
+
+	for (a = 0; src[a] != '\0'; ++a)
+	{
+		dest[b] = src[a];
+		++b;
+	}
+	dest[b] = '\0';
+
+	return (dest);
 }
