2af8f4d842de738c6642e6c27a8a3f45609f39dd
Jonazy <jonas.humenu@gmail.com>
2023-03-01 14:38:30 -0800

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,22 +12,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int c, i;
 
-	i = 0;
-	j = 0;
+	c = 0;
 
-	while (dest[i] != '\0')
-		i++;
+	while (dest[c])
+		c++;
 
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
 
-	dest[i] = '\0';
+	dest[c + i] = '\0';
 
 	return (dest);
 }
