f9954318fc965b9924a866508f71f76a958be4ee
Jonazy <jonas.humenu@gmail.com>
2023-02-28 17:43:59 -0800

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: argument
+ *
+ * Return: length of a sting
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
