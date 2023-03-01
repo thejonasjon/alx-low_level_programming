bbbbf8d58105a14a48258a9ce937a8b3c2670335
Jonazy <jonas.humenu@gmail.com>
2023-02-28 18:21:56 -0800

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include"main.h"
-#include <stdio.h>  
-#include <string.h>
+#include <stdio.h> 
 
 /**
  * print_rev - function that prints a string,
@@ -13,6 +12,21 @@
 
 void print_rev(char *s)
 {
-	printf("%c\n", strrev(s));
-	return (0);
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
 }
