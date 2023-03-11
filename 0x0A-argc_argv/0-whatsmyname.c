90dc5adf0be8cecbbb869302918c0bf930605c8a
Jonazy <jonas.humenu@gmail.com>
2023-03-10 18:17:48 -0800

    first comit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * main - a program to prints the program name, followed by a new line.
+ * @argc:first argument
+ * @argv: second argument
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
