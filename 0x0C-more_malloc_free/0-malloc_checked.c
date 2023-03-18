8392d866bac64186a945486d764eb8aede5eca5c
Jonazy <jonas.humenu@gmail.com>
2023-03-17 21:14:00 -0700

    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *new_mem;
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	new_mem = malloc(b);
+	if (new_mem == NULL)
 		exit(98);
-
-	return (ptr);
+	return (new_mem);
 }
